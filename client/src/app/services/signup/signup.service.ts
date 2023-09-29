742abf458aa351bf39c1a2fd3668a4908ca30fb7
Luks245 <florekl03c@lo5.bielsko.pl>
2023-09-29 13:45:20 +0200

    modified budget limits

--- client/src/app/services/signup/signup.service.ts
+++ client/src/app/services/signup/signup.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
-export const BACKEND_URL = 'http://socially-awkward.free.nf/api/';
+export const BACKEND_URL = 'http://socially-awkward.free.nfvert/api/';
 
 @Injectable({
   providedIn: 'root',
